9f2e9cdc1bfa722df3525918d4a717708a534574
张少鹏-移动技术部 <zhangshaopeng@wtoip.com>
2018-05-03 17:03:17 +0800

    注释

--- app/src/main/java/com/shaopeng/utilsdemo/MainActivity.java
+++ app/src/main/java/com/shaopeng/utilsdemo/MainActivity.java
@@ -3,9 +3,9 @@ package com.shaopeng.utilsdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 /**
- * Description:整理一些用到的工具类，方便以后使用
- * Company:
- * Author:Zhangshaopeng
+ * Description:Sort out some tool classes to make it easy to make
+ * Company:(secrecy)
+ * Author:zhangshaopeng
  * Email :1377785991@qq.com
  * Data:2018/5/3
  */
